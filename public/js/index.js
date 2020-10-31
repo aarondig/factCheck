8b8749fb26146469d13b6e8c32fbd0a4b28b8cf4
Aaron Diggdon <aarondiggdon@gmail.com>
2020-10-30 19:46:45 -0700

    Fixed multiple clicks bug on main search button

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
 
+    $(document).on('keypress', function(enter) {
+        if (enter.which == 13) {
+            var userInput = $("#search").val().trim();
+            var search = encodeURIComponent(userInput);
+            console.log(search);
+        }
+    });
+
+
+    // CARD FLIP
+
     var $num = $('.cardContainer .card').length;
 
     var $even = $num / 2;
@@ -44,4 +55,57 @@ $(document).ready(function() {
             $('.cardContainer .active').next().trigger('click');
         }
     });
+});
+
+
+
+var timesClicked = 1;
+
+$(".search").click(function(e) {
+    timesClicked++;
+    console.log(timesClicked);
+    if (timesClicked % 2 == 0) {
+        async function display() {
+            document.getElementById("search").style.display = "inline-block";
+        }
+        display().then(function() {
+            $("#search").focus();
+        });
+        display();
+        // Remove any old one
+
+        // Setup
+        var posX = $(this).offset().left,
+            posY = $(this).offset().top,
+            buttonWidth = $(this).width(),
+            buttonHeight = $(this).height();
+
+        // Add the element
+        $(".search").append("<span class='ripple'></span>");
+
+        // Make it round!
+        if (buttonWidth >= buttonHeight) {
+            buttonHeight = buttonWidth;
+        } else {
+            buttonWidth = buttonHeight;
+        }
+
+        // Get the center of the element
+        var x = e.pageX - posX - buttonWidth / 2;
+        var y = e.pageY - posY - buttonHeight / 2;
+
+
+        // Add the ripples CSS and start the animation
+        $(".ripple").css({
+            width: buttonWidth,
+            height: buttonHeight,
+            top: y + 'px',
+            left: x + 'px'
+        }).addClass("rippleEffect");
+
+    } else {
+        $(".ripple").remove();
+        document.getElementById("search").style.display = "none";
+        console.log("removed");
+    }
 });
